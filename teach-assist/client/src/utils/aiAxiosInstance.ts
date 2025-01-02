1cd4c8710f9f6e948e9f28a300020b0c74d37a7a
Victor <vagarciay@gmail.com>
2025-01-01 20:12:30 -0600

    fix(frontend): update AI API URL to use production endpoint

--- teach-assist/client/src/utils/aiAxiosInstance.ts
+++ teach-assist/client/src/utils/aiAxiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const aiAxiosInstance = axios.create({
-  baseURL: 'http://localhost:5001',
+  baseURL: process.env.REACT_APP_AI_URL || 'http://localhost:5001',
   headers: {
     'Content-Type': 'application/json',
   },
